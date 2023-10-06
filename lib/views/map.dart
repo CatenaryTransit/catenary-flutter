c4eb087b8c27ea7568d691c58ac205a12b1d856a
AndrewHC36 <andrewshen31@gmail.com>
2023-10-06 07:52:32 -0700

    compass fixed

--- lib/views/map.dart
+++ lib/views/map.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:maplibre_gl/mapbox_gl.dart';
 import 'package:location/location.dart';
@@ -10,14 +12,29 @@ class CatenaryMapView extends StatefulWidget {
 }
 
 class _CatenaryMapViewState extends State<CatenaryMapView> {
-  late MaplibreMapController mapController;
-  Location location = new Location();
+  MaplibreMapController? mapController;
+  Location location = Location();
 
   late bool _serviceEnabled;
   late PermissionStatus _permissionGranted;
-  late LocationData? _locationData;
+  LocationData? _locationData;
 
   Future<void> _onMapCreated(MaplibreMapController controller) async {
+    print("ON MAP CREATED");
+
+    setState(() {
+      mapController = controller;
+      mapController!.addCircle(CircleOptions(
+        circleRadius: 10,
+        circleColor: '#2563EB',
+        circleOpacity: 1,
+        circleStrokeWidth: 1,
+        circleStrokeColor: '#ffffff',
+        geometry: LatLng(
+            _locationData?.latitude ?? 0.0, _locationData?.longitude ?? 0.0),
+      ));
+    });
+
     _serviceEnabled = await location.serviceEnabled();
     if (!_serviceEnabled) {
       _serviceEnabled = await location.requestService();
@@ -34,30 +51,84 @@ class _CatenaryMapViewState extends State<CatenaryMapView> {
       }
     }
     location.enableBackgroundMode(enable: true);
-    _locationData = await location.getLocation();
-    mapController = controller;
-    mapController.addCircle(CircleOptions(
-      circleRadius: 10,
-      circleColor: '#2563EB',
-      circleOpacity: 1,
-      circleStrokeWidth: 1,
-      circleStrokeColor: '#ffffff',
-      geometry: LatLng(
-          _locationData?.latitude ?? 0.0, _locationData?.longitude ?? 0.0),
-    ));
+    print("PERMISSION PASSED?");
+    setState(() async {
+      _locationData = await location.getLocation();
+    });
+;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: MaplibreMap(
-          onMapCreated: _onMapCreated,
-          compassEnabled: false,
-          styleString:
-              'https://api.maptiler.com/maps/68c2a685-a6e4-4e26-b1c1-25b394003539/style.json?key=tf30gb2F4vIsBW5k9Msd',
-          initialCameraPosition: const CameraPosition(
-              target: LatLng(33, -117), zoom: 7.0, bearing: 0.0, tilt: 0.0),
-          trackCameraPosition: true,
+        body: Listener(
+          // onPointerDown: (PointerDownEvent event) {
+          //   print("CLICK!!");
+          //   if(event.buttons == kSecondaryButton) {
+          //     print("RIGHT CLICK??");
+          //   }
+          // },
+          child: RawGestureDetector(
+            // gestures: <Type, GestureRecognizerFactory>{
+            //   // ImmediateMultiDragGestureRecognizer: GestureRecognizerFactoryWithHandlers<ImmediateMultiDragGestureRecognizer>(
+            //   //       () => ImmediateMultiDragGestureRecognizer(),
+            //   //       (ImmediateMultiDragGestureRecognizer inst) {
+            //   //         inst.onStart = (Offset pos) {
+            //   //           print("Immediate Multi Drag $pos");
+            //   //         };
+            //   //       },
+            //   // ),
+            //   TapGestureRecognizer: GestureRecognizerFactoryWithHandlers<TapGestureRecognizer>(
+            //       () => TapGestureRecognizer(),
+            //       (TapGestureRecognizer inst) {
+            //         inst.onTap = () {
+            //           print("=== RIGHT CLICK?? 1");
+            //         };
+            //         inst.onTapDown = (TapDownDetails det) {
+            //           print("=== RIGHT CLICK?? 2");
+            //         };
+            //         inst.onSecondaryTap = () {
+            //           print("=== RIGHT CLICK?? 3");
+            //         };
+            //         inst.onSecondaryTapDown = (TapDownDetails det) {
+            //           print("=== RIGHT CLICK?? 4");
+            //         };
+            //         inst.onTertiaryTapDown = (TapDownDetails det) {
+            //           print("=== RIGHT CLICK?? 5");
+            //         };
+            //         inst.onTertiaryTapUp = (TapUpDetails det) {
+            //           print("=== RIGHT CLICK?? 6");
+            //         };
+            //       },
+            //   ),
+            //   PanGestureRecognizer: GestureRecognizerFactoryWithHandlers<PanGestureRecognizer>(
+            //       () => PanGestureRecognizer(),
+            //       (PanGestureRecognizer inst) {
+            //         inst.onUpdate = (DragUpdateDetails details) {
+            //           print("Pan ${details.delta}");
+            //           moveCamera(details.delta.dx, details.delta.dy);
+            //         };
+            //       },
+            //   ),
+            // },
+            child: MaplibreMap(
+              // gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+              //   Factory<OneSequenceGestureRecognizer>(() => EagerGestureRecognizer(),),
+              //   // Factory<PanG>(() => EagerGestureRecognizer(),),
+              // },
+              onMapCreated: _onMapCreated,
+              compassEnabled: false,
+              styleString:
+                  'https://api.maptiler.com/maps/68c2a685-a6e4-4e26-b1c1-25b394003539/style.json?key=tf30gb2F4vIsBW5k9Msd',
+              initialCameraPosition: const CameraPosition(
+                  target: LatLng(33, -117), zoom: 7.0, bearing: 0.0, tilt: 0.0),
+              trackCameraPosition: true,
+              // tiltGesturesEnabled: true,
+              // zoomGesturesEnabled: true,
+              // scrollGesturesEnabled: true,
+              // rotateGesturesEnabled: true,
+            ),
+          ),
         ),
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
@@ -67,10 +138,8 @@ class _CatenaryMapViewState extends State<CatenaryMapView> {
             ),
             FloatingActionButton(
               onPressed: () {
-                _locationData != null
-                    ? mapController.animateCamera(CameraUpdate.bearingTo(0.0),
-                        duration: const Duration(seconds: 2))
-                    : null;
+                print("MAP CTRL $mapController");
+                mapController!.animateCamera(CameraUpdate.bearingTo(0.0), duration: const Duration(seconds: 2));
               },
               foregroundColor: Colors.white,
               backgroundColor: const Color(0xFF0a233f),
@@ -82,16 +151,22 @@ class _CatenaryMapViewState extends State<CatenaryMapView> {
             const Spacer(),
             FloatingActionButton(
               onPressed: () {
-                _locationData != null
-                    ? mapController.animateCamera(
-                        CameraUpdate.newCameraPosition(CameraPosition(
-                            target: LatLng(_locationData?.latitude ?? 0.0,
-                                _locationData?.longitude ?? 0.0),
-                            zoom: 11.0,
-                            bearing: 0.0,
-                            tilt: 0.0)),
-                        duration: const Duration(seconds: 2))
-                    : null;
+                if(_locationData != null) {
+                  mapController!.animateCamera(
+                    CameraUpdate.newCameraPosition(
+                      CameraPosition(
+                        target: LatLng(
+                            _locationData?.latitude ?? 0.0,
+                            _locationData?.longitude ?? 0.0
+                        ),
+                        zoom: 11.0,
+                        bearing: 0.0,
+                        tilt: 0.0,
+                      ),
+                    ),
+                    duration: const Duration(seconds: 2),
+                  );
+                }
               },
               foregroundColor: Colors.white,
               backgroundColor: const Color(0xFF0a233f),
@@ -100,6 +175,7 @@ class _CatenaryMapViewState extends State<CatenaryMapView> {
               child: const Icon(Icons.my_location),
             ),
           ],
-        ));
+        )
+    );
   }
 }
